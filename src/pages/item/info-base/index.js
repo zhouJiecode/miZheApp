46cac8dfb4ba6c20d71b55b0f1e334ea0edaa4f6
zhoujie@yiwise.com <zhoujie@yiwise.com>
2019-07-05 19:04:19 +0800

    分享

--- src/pages/item/info-base/index.js
+++ src/pages/item/info-base/index.js
@@ -1,5 +1,7 @@
 import Taro from '@tarojs/taro'
 import { View, Text } from '@tarojs/components'
+import { AtIcon } from 'taro-ui'
+import { ActionSheet } from '@components'
 import './index.scss'
 
 export default class InfoBase extends Taro.PureComponent {
@@ -7,12 +9,48 @@ export default class InfoBase extends Taro.PureComponent {
     data: {}
   }
 
+  state = {
+    actionSheetVisible: false
+  }
+
+  handleActionSheetClose() {
+    this.setState({
+      actionSheetVisible: false
+    })
+  }
+
+  openActionSheet() {
+    this.setState({
+      actionSheetVisible: true
+    })
+  }
+
   render () {
     const { data } = this.props
     const { itemStar = {} } = data
+    const { actionSheetVisible } = this.state
 
     return (
       <View className='item-info-base'>
+
+        <View className='item-info-base__price'>
+          <View className='item-info-base__price-wrap'>
+            <Text className='item-info-base__price-symbol'>¥</Text>
+            <Text className='item-info-base__price-txt'>
+              {data.activityPrice || data.retailPrice}
+            </Text>
+            {!!data.activityPrice &&
+              <Text className='item-info-base__price-origin'>
+                ¥{data.retailPrice}
+              </Text>
+            }
+          </View>
+          <View className='item-info-base__share' onClick={this.openActionSheet.bind(this)}>
+            <AtIcon customStyle='' value='share-2' size='20' color='#666'></AtIcon>
+            <Text className='item-info-base__share-txt' style='font-size: 8px;'>分享</Text>
+          </View>
+        </View>
+
         <View className='item-info-base__header'>
           <View className='item-info-base__header-wrap'>
             <Text className='item-info-base__header-name'>{data.name}</Text>
@@ -26,17 +64,10 @@ export default class InfoBase extends Taro.PureComponent {
           </View>
         </View>
 
-        <View className='item-info-base__price'>
-          <Text className='item-info-base__price-symbol'>¥</Text>
-          <Text className='item-info-base__price-txt'>
-            {data.activityPrice || data.retailPrice}
-          </Text>
-          {!!data.activityPrice &&
-            <Text className='item-info-base__price-origin'>
-              ¥{data.retailPrice}
-            </Text>
-          }
-        </View>
+        <ActionSheet
+          visible={actionSheetVisible}
+          onClose={this.handleActionSheetClose.bind(this)}
+        />
 
         {/* {!!tagList.length &&
           <View className='item-info-base__tag'>
